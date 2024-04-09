6cd6166c2c78a0e5f29aaa96f7ab7c5ef05d63cf
yokotaerik <yokota082003@gmail.com>
2024-04-09 10:53:08 -0300

    Adicionando resposividade na navbar

--- frontend/src/components/home/header/components/Navbar.tsx
+++ frontend/src/components/home/header/components/Navbar.tsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import logo from '../../../../images/logo.png';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleNavbarMobile = () => {
+        if(isOpen){
+            setIsOpen(false)
+        } else {
+            setIsOpen(true)
+        }
+
+    }
+
     return (
         <nav className="bg-amber-900 py-7">
             <div className="px-10 flex justify-between items-center">
@@ -9,20 +21,24 @@ export default function Navbar() {
                     <img src={logo} alt="Logo" className="h-8 w-8 mr-2"/>
                 </div>
                 
-                <div className="flex items-center space-x-4 hidden md:block">
+                <div className="items-center space-x-4 hidden md:flex">
                     <a href="/" className="text-white hover:text-gray-300">Inicio</a>
                     <a href="/schedule" className="text-white hover:text-gray-300">Agendamento</a>
                     <a href="/contact" className="text-white hover:text-gray-300">Contato</a>
                     <a href="/register" className="inline-block bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600">Login</a>
                 </div>
-
-                <a href="/register" className="inline-block bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 block md:hidden">Login</a>
+                <button className='md:hidden' onClick={handleNavbarMobile}>X</button>
             </div>
-                <div className="flex flex-col items-center space-x-4 block md:hidden">
+
+                { isOpen ? (
+                <div className="flex flex-col items-center space-x-4  md:hidden">
+                <a href="/register" className="inline-block bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 md:hidden">Login</a>
                     <a href="/" className="text-white hover:text-gray-300">Inicio</a>
                     <a href="/schedule" className="text-white hover:text-gray-300">Agendamento</a>
                     <a href="/contact" className="text-white hover:text-gray-300">Contato</a>
                 </div>
+                ) : null
+                }
         </nav>
     )
 }
