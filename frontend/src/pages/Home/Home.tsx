f4c71dd5479641b507d4efab0f2ef44d3486f651
thepokenik <nikolaspedro.np@gmail.com>
2024-08-12 14:25:48 -0300

    feat: Add homepage with background image

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home () {
+    return (
+      <>
+      <img src="https://images.pexels.com/photos/3992874/pexels-photo-3992874.jpeg" alt="Barbearia" className="w-full h-screen object-cover brightness-75" />
+      </>
+    )
+}
