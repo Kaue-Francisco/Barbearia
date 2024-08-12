00b7cdd60089a31e6eb1a07f7cd429d233ea1a21
thepokenik <nikolaspedro.np@gmail.com>
2024-08-12 17:26:49 -0300

    refactor: Update navbar styles and links

--- frontend/src/pages/Components/Navbar/Navbar.tsx
+++ frontend/src/pages/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Link, useLocation } from "react-router-dom";
-import { Icon, Menu } from 'lucide-react';
+import { Menu } from 'lucide-react';
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { cn } from "@/lib/utils";
 import logo from "@/assets/logo.png";
@@ -10,7 +10,7 @@ export default function Navbar() {
     const pathname = location.pathname;
 
     return (
-        <header className="flex h-16 w-full items-center justify-between bg-primary/90 px-4 md:px-6">
+        <header className="flex h-16 w-full items-center justify-between bg-foreground/90 px-4 md:px-6">
             <Link to="/" className="mr-6 flex items-center space-x-2">
                 <img src={logo} alt={"Logo"} width={42} />
                 <span className="hidden font-bold sm:inline-block text-secondary/90">
@@ -31,12 +31,12 @@ export default function Navbar() {
                     to="/docs"
                     className={cn(
                         "font-bold transition-colors hover:text-secondary",
-                        pathname.startsWith("/docs") ? "text-secondary" : "text-secondary/90"
+                        pathname.startsWith("/contato") ? "text-secondary" : "text-secondary/90"
                     )}
                 >
                     Contato
                 </Link>
-                <Button className="bg-primary font-bold" asChild><Link to="/auth/register">Agendamento</Link></Button>
+                <Button className="bg-primary font-bold" asChild><Link to="/agendamento">Agendamento</Link></Button>
             </nav>
             {/* Mobile Menu */}
             <Sheet>
@@ -52,7 +52,15 @@ export default function Navbar() {
                 </SheetTrigger>
                 <SheetContent side="right">
                     <nav className="grid gap-6 text-lg font-medium">
-
+                        <MobileLink to="/" className="hover:text-secondary">
+                            Inicio
+                        </MobileLink>
+                        <MobileLink to="/contato" className="hover:text-secondary">
+                            Contato
+                        </MobileLink>
+                        <MobileLink to="/agendamento" className="hover:text-secondary">
+                            Agendamento
+                        </MobileLink>
                     </nav>
                 </SheetContent>
             </Sheet>
@@ -66,6 +74,11 @@ function MobileLink({
     className,
     children,
     ...props
+}: {
+    to: string;
+    onOpenChange?: (open: boolean) => void;
+    className: string;
+    children: React.ReactNode;
 }) {
     return (
         <Link
