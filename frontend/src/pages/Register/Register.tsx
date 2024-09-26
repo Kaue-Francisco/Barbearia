2be00ad4acc31ddea583818154dcc09e556fd70e
Kaue-Francisco <kauesantos.francisco@gmail.com>
2024-09-25 22:29:37 -0300

    fe-feat: Alterando estrutura da página para fazer integração com o back.

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -2,7 +2,9 @@ import image_register_page from '@/assets/placa_cia_bigode.jpeg'
 import image_logo from '@/assets/logo.png'
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 import { useState } from 'react';
+import bcrypt from 'bcryptjs';
 
 export default function Registro() {
   const [formData, setFormData] = useState({
@@ -13,6 +15,7 @@ export default function Registro() {
     confirm_password: ''
   });
   const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -22,19 +25,52 @@ export default function Registro() {
     setError(''); // Clear error message on input change
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (formData.password !== formData.confirm_password) {
       setError('As senhas não coincidem!');
       return;
+    }
+
+    setIsSubmitting(true);
+    try {
+      const hashedPassword = await bcrypt.hash(formData.password, 10);
+      console.log(hashedPassword);
+      const payload = {
+        data: {
+          name: formData.name,
+          phone_number: formData.phone_number,
+          email: formData.email,
+          password: formData.password,
+        },
+      };
+
+      const response = await fetch('http://localhost:5000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao registrar');
+      }
+
+      // Simulate registration process
+      await new Promise(resolve => setTimeout(resolve, 2000));
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
   return (
     <div className="grid grid-cols-[45%,55%]">
       <div className='flex flex-col justify-center items-center col-span-1 h-screen w-screen md:w-auto bg-background'>
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight mb-6 lg:text-5xl">Registre-se</h1>
-        <img className='md:w-[75px] md:h-[58px] w-[60px] h-[60px] mb-6' src={image_logo} />
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight mb-6 lg:text-5xl select-none">Registre-se</h1>
+        <img className=' md:w-[75px] md:h-[58px] w-[60px] h-[60px] mb-6 select-none' src={image_logo} />
         <form className='flex flex-col md:w-3/5' onSubmit={handleSubmit}>
           <Label htmlFor='name' className='mt-4 mb-1'>Nome completo:</Label>
           <Input required id='name' type='text' placeholder='Digite seu nome: ' value={formData.name} onChange={handleChange} />
@@ -53,8 +89,10 @@ export default function Registro() {
           
           {error && <p className='text-red-500 text-sm mt-2'>{error}</p>}
           
-          <button className='text-sm text-white mt-8 bg-orange-400 rounded-md md:w-auto font h-10'>Confirmar</button>
-          <p className='text-center text-sm'>Já possui conta? Faça login <a className='text-blue-400' href='/login'>aqui!</a></p>
+          <Button className="bg-primary font-bold px-6 mt-4 select-none" type="submit" disabled={isSubmitting}>
+            {isSubmitting ? 'Registrando...' : 'Registrar'}
+          </Button>
+          <p className='text-center text-sm select-none'>Já possui conta? Faça login <a className='text-blue-400' href='/login'>aqui!</a></p>
         </form>
       </div>
       <div className='md:flex bg-cover bg-center bg-no-repeat md:col-span-1 hidden brightness-50' style={{ backgroundImage: `url(${image_register_page})`}} />
